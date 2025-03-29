129589a379ad85e96e1e7172618363a03cb07e77
LyenZo <marcosj.9807@gmail.com>
2025-03-29 09:20:11 -0600

    documentacion

--- app.py
+++ app.py
@@ -1,32 +1,40 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from config import db, migrate
 from dotenv import load_dotenv
 import os
 from flask_cors import CORS 
 from flask_jwt_extended import JWTManager
+from flask_swagger_ui import get_swaggerui_blueprint
 
 load_dotenv()
 
 app = Flask(__name__)
 
-
 CORS(app, resources={r"/users/*": {"origins": "*"}}, supports_credentials=True)  
-
 
 app.config['JWT_SECRET_KEY'] = 'qwertyuiop'
 jwt = JWTManager(app)
 
-
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 
 db.init_app(app)
 migrate.init_app(app, db)
 
-
 from routes.user import user_bp
 app.register_blueprint(user_bp, url_prefix='/users')
+
+# Configuración Swagger UI
+SWAGGER_URL = '/apidocs'
+API_URL = '/static/swagger.yaml'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL, 
+    API_URL,
+    config={
+        'app_name': "Mi API"
+    }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == '__main__':
     app.run(debug=True)
